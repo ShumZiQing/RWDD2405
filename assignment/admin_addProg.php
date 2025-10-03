d7595dae02118c46cd40da508ee3d081e380304f
LXFoon <laufoonxuan@gmail.com>
2025-10-03 17:56:01 +0800

    added editProg, addProg

---
+++ assignment/admin_addProg.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Program | Admin</title>
+    <link rel="stylesheet" href="styles/admin_prog.css">
+    <link rel="stylesheet" href="styles/global.css">
+
+    <style>
+        #indiForm, #indiTxtArea, #indiSmallForm{
+            font-style: normal;
+            color: #8B8B8B;
+        }
+
+        #indiSel{
+            font-style: normal;
+        }
+    </style>
+</head>
+<body>
+    <?php include "admin_header.php"?>
+
+    <div id="side">
+        <?php include "admin_sideMenu.php"?>
+    </div>
+
+    <div id="content">
+        <h1>Add Program</h1>
+
+    <div id="box">
+        <div class="forms">
+            <div id="circle"></div>
+            <form action="#" method="post">
+                <input type="text" name="txtStartDate" value="Start Date" id="indiSmallForm">
+                to
+                <input type="text" name="txtEndDate" value="End Date" id="indiSmallForm">
+            </form>
+        </div>
+
+        <div class="forms">
+            <div id="circle"></div>
+            <form action="#" method="post">
+                <input type="text" name="txtStartTime" value="Start Time" id="indiSmallForm">
+                to
+                <input type="text" name="txtEndTime" value="End Time" id="indiSmallForm">
+            </form>
+        </div>
+
+        <div class="forms">
+            <div id="circle"></div>
+            <form action="#" method="post">
+                <textarea name="txtProgDetails" id="indiTxtArea">Program Details</textarea>
+            </form>
+        </div>
+
+        <div class="forms">
+            <div id="circle"></div>
+            <form action="#" method="post">
+                <select name="selRecyclable" id="indiSel">
+                    <option value="PLT">Plastic</option>
+                    <option value="MTL">Metal</option>
+                    <option value="PPR">Paper</option>
+                    <option value="GLS">Glass</option>
+                </select>
+            </form>
+        </div>
+
+        <div class="forms">
+            <div id="circle"></div>
+            <form action="#" method="post">
+                <select name="selRecyclable" id="indiSel">
+                    <option value="ESP">Taman Esplanad</option>
+                    <option value="LTAT">Taman LTAT</option>
+                    <option value="PUJ">Taman Puncak Jalil</option>
+                    <option value="YAR">Taman Yarl</option>
+                    <option value="EQP">Taman Equine Park</option>
+                </select>
+            </form>
+        </div>
+
+        <div class="forms">
+            <div id="circle"></div>
+            <form action="#" method="post">
+                <select name="selFrequency" id="indiSel">
+                    <option value="day">Daily</option>
+                    <option value="week">Weekly</option>
+                    <option value="month">Monthly</option>
+                </select>
+            </form>
+        </div>
+
+        <div class="forms">
+            <div id="circle"></div>
+            <form action="#" method="post">
+                <select name="selCollab" id="indiSel">
+                    <option value="ESGAM">ESG Association of Malaysia</option>
+                    <option value="MAREA">Malaysian Recycling Alliance</option>
+                    <option value="PERKESA">Malaysian Recycling Empowerment Association </option>
+                    <option value="PASS">Pertubuhan Amal Seri Sinar</option>
+                </select>
+            </form>
+        </div>
+
+        <input type="submit" value="Save" id="save">
+    </div>
+    </div>
+</body>
+</html>
