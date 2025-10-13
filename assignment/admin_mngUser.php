1e728277f2fa84a9f70c75875952323d2955ecfc
sinyi97 <sinyichang97@gmail.com>
2025-10-14 07:29:56 +0800

    Add files via upload

---
+++ assignment/admin_mngUser.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manage User | Admin</title>
+
+    <link rel="stylesheet" href="styles/mngUser.css">
+    <link rel="stylesheet" href="styles/global.css">
+
+    <script src="https://kit.fontawesome.com/b70fe5a297.js" crossorigin="anonymous"></script>
+</head>
+</head>
+<body>
+
+    <?php include 'admin_header.php'?>
+
+    <div id="side">
+        <?php include 'admin_sideMenu.php'?>
+    </div>
+
+    <div id="title">
+        <p>Manage User</p>
+    </div>
+
+    <div id="search">
+        <input type="text" name="search" placeholder="Search User">
+        <i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i>
+    </div>
+
+    <div id="user">
+        <div class="container">
+            <i class="fa-solid fa-user profile"></i>
+
+            <div class="text">
+                <p id="userid">User ID</p>
+                <p id="username">Username</p>
+            </div>
+
+            <div id="icon">
+                <i class="fa-solid fa-eye view"></i>
+                <i class="fa-solid fa-trash delete"></i>
+            </div>
+        </div>
+
+        <div class="container">
+            <i class="fa-solid fa-user profile"></i>
+
+            <div class="text">
+                <p id="userid">User ID</p>
+                <p id="username">Username</p>
+            </div>
+
+            <div id="icon">
+                <i class="fa-solid fa-eye view"></i>
+                <i class="fa-solid fa-trash delete"></i>
+            </div>
+        </div>
+
+        <div class="container">
+            <i class="fa-solid fa-user profile"></i>
+
+            <div class="text">
+                <p id="userid">User ID</p>
+                <p id="username">Username</p>
+            </div>
+
+            <div id="icon">
+                <i class="fa-solid fa-eye view"></i>
+                <i class="fa-solid fa-trash delete"></i>
+            </div>
+        </div>
+
+        <div class="container">
+            <i class="fa-solid fa-user profile"></i>
+
+            <div class="text">
+                <p id="useid">User ID</p>
+                <p id="username">Username</p>
+            </div>
+
+            <div id="icon">
+                <i class="fa-solid fa-eye view"></i>
+                <i class="fa-solid fa-trash delete"></i>
+            </div>
+        </div>
+
+        <div class="container">
+            <i class="fa-solid fa-user profile"></i>
+
+            <div class="text">
+                <p id="userid">User ID</p>
+                <p id="username">Username</p>
+            </div>
+
+            <div id="icon">
+                <i class="fa-solid fa-eye view"></i>
+                <i class="fa-solid fa-trash delete"></i>
+            </div>
+        </div>
+
+        <div class="container">
+            <i class="fa-solid fa-user profile"></i>
+
+            <div class="text">
+                <p id="userid">User ID</p>
+                <p id="username">Username</p>
+            </div>
+
+            <div id="icon">
+                <i class="fa-solid fa-eye view"></i>
+                <i class="fa-solid fa-trash delete"></i>
+            </div>
+        </div>
+
+        <div class="container">
+            <i class="fa-solid fa-user profile"></i>
+
+            <div class="text">
+                <p id="userid">User ID</p>
+                <p id="username">Username</p>
+            </div>
+
+            <div id="icon">
+                <i class="fa-solid fa-eye view"></i>
+                <i class="fa-solid fa-trash delete"></i>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
