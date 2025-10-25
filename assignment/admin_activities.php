00081f3172e53bf555f10ad38f16496c7acee025
Chang Sin Yi <sinyichang97@gmail.com>
2025-10-25 17:08:05 +0800

    activities

--- assignment/admin_activities.php
+++ assignment/admin_activities.php
@@ -1,20 +1,21 @@
 <?php
-    include 'dbConn.php';
-    session_start();
+include 'dbConn.php';
+session_start();
 
-    if(isset($_GET['btnSearch'])){
-        $search = $_GET['txtSearch'];
-        $progSql = "SELECT * FROM tblprograms WHERE progName LIKE '%$search%'";
-        $projSql = "SELECT * FROM tblprojects WHERE prjName LIKE '%$search%'";
+if (isset($_GET['btnSearch'])) {
+    $search = $_GET['txtSearch'];
+    $progSql = "SELECT * FROM tblprograms WHERE progName LIKE '%$search%'";
+    $projSql = "SELECT * FROM tblprojects WHERE prjName LIKE '%$search%'";
 
-    }else{
-        $progSql = "SELECT * FROM tblprograms";
-        $projSql = "SELECT * FROM tblprojects";
-    }
+} else {
+    $progSql = "SELECT * FROM tblprograms";
+    $projSql = "SELECT * FROM tblprojects";
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -26,7 +27,7 @@
 
 <body>
     <?php include "admin_header.php" ?>
-    
+
     <div id="content">
         <div id="side">
             <?php include "admin_sideMenu.php" ?>
@@ -37,9 +38,11 @@
             <h1>All Activities</h1>
             <div id="features">
                 <div id="search">
-                    <form action="" method = "get">
-                        <input type="text" name="txtSearch" placeholder="Search program" id="searchBar" autocomplete = "off">
-                        <button type="submit" name="btnSearch" id="searchButton"><img src="images/magnifying-glass.png" alt="search" class="icon"></button>
+                    <form action="" method="get">
+                        <input type="text" name="txtSearch" placeholder="Search program" id="searchBar"
+                            autocomplete="off">
+                        <button type="submit" name="btnSearch" id="searchButton"><img src="images/magnifying-glass.png"
+                                alt="search" class="icon"></button>
                     </form>
                 </div>
                 <div id="add">
@@ -47,37 +50,42 @@
                     <p>Add</p>
                     <img src="images/down-arrow.png" alt="dropdown" class="icon">
                     <ul>
-                        <a href="admin_addProg.php"><li>New Program</li></a>
-                        <a href="admin_addPrj.php"><li>New Project</li></a>
+                        <a href="admin_addProg.php">
+                            <li>New Program</li>
+                        </a>
+                        <a href="admin_addPrj.php">
+                            <li>New Project</li>
+                        </a>
                     </ul>
                 </div>
             </div>
 
             <?php
-                $progResult = mysqli_query($conn, $progSql);
+            $progResult = mysqli_query($conn, $progSql);
 
-                while($progRow = mysqli_fetch_assoc($progResult)){
-                    $progID = $progRow['progID'];
-                    $progName = $progRow['progName'];
-                    $progDesc = $progRow['progDetails'];
-                    ?>
+            while ($progRow = mysqli_fetch_assoc($progResult)) {
+                $progID = $progRow['progID'];
+                $progName = $progRow['progName'];
+                $progDesc = $progRow['progDetails'];
+                ?>
 
-                    <div class="prog">
-                        <?php if (!empty($progRow['progImage'])): ?>
-                        <img src="images/<?= htmlspecialchars($progRow['progImage']) ?>"  alt="picture" id="picture">
-                        <?php endif; ?>
+                <div class="prog">
+                    <?php if (!empty($progRow['progImage'])): ?>
+                        <img src="images/<?= htmlspecialchars($progRow['progImage']) ?>" alt="picture" id="picture">
+                    <?php endif; ?>
                     <div id="desc">
-                        <h2><?php echo $progName?></h2>
-                        <p><?php echo $progDesc?></p>
+                        <h2><?php echo $progName ?></h2>
+                        <p><?php echo $progDesc ?></p>
                     </div>
-                    
+
                     <div class="actIcon">
                         <div id="collab">
-                            <a href="admin_collabDetail.php?collabName=<?php echo $progRow['collabName'];?>"><i class="fa-solid fa-users fa-xl collabIcon"></i></a>
+                            <a href="admin_collabDetail.php?collabName=<?php echo $progRow['collabName']; ?>"><i
+                                    class="fa-solid fa-users fa-xl collabIcon"></i></a>
                             <p>Collaborator</p>
                         </div>
-                        
-                        <a href="admin_editProg.php?progID=<?php echo $progRow['progID'];?>">
+
+                        <a href="admin_editProg.php?progID=<?php echo $progRow['progID']; ?>">
                             <div id="edit">
                                 <i class="fa-solid fa-pen-to-square fa-xl editIcon"></i>
                                 <p>Edit</p>
@@ -86,48 +94,51 @@
                     </div>
                 </div>
 
-            <?php
-                }
+                <?php
+            }
 
             ?>
 
             <?php
-                $projResult = mysqli_query($conn, $projSql);
+            $projResult = mysqli_query($conn, $projSql);
 
-                while($projRow = mysqli_fetch_assoc($projResult)){
-                    $projID = $projRow['prjID'];
-                    $projName = $projRow['prjName'];
-                    $projDesc = $projRow['prjDetails'];
-                    $projImg = $projRow['prjImg'];
-                    ?>
+            while ($projRow = mysqli_fetch_assoc($projResult)) {
+                $projID = $projRow['prjID'];
+                $projName = $projRow['prjName'];
+                $projDesc = $projRow['prjDetails'];
+                $projImg = $projRow['prjImg'];
+                ?>
 
-                    <div class="proj">
-                        <img src="<?php echo $projImg?>" alt="picture" id="picture">
-                    <div id="desc">
-                        <h2><?php echo $projName?></h2>
-                        <p><?php echo $projDesc?></p>
+                <div class="prj">
+                    <?php if (!empty($projRow['prjImage'])): ?>
+                        <img src="images/<?= htmlspecialchars($progRow['prjImage']) ?>" alt="picture" id="picture">
+                    <?php endif; ?>
+                    <h2><?php echo $projName ?></h2>
+                    <p><?php echo $projDesc ?></p>
+                </div>
+
+                <div class="actIcon">
+                    <div id="collab">
+                        <a href="admin_viewPcp.php?prjID=<?php echo $projRow['prjID']; ?>"><i
+                                class="fa-solid fa-users-line fa-2xl pcpIcon"></i></a>
+                        <p>Participants</p>
                     </div>
-                    
-                    <div class="actIcon">
-                        <div id="collab">
-                            <a href="admin_viewPcp.php?prjID=<?php echo $projRow['prjID'];?>"><i class="fa-solid fa-users-line fa-2xl pcpIcon"></i></a>
-                            <p>Participants</p>
+
+                    <a href="admin_editPrj.php?prjID=<?php echo $projRow['prjID']; ?>">
+                        <div id="edit">
+                            <i class="fa-solid fa-pen-to-square fa-xl editIcon"></i>
+                            <p>Edit</p>
                         </div>
-                        
-                        <a href="admin_editPrj.php?prjID=<?php echo $projRow['prjID'];?>">
-                            <div id="edit">
-                                <i class="fa-solid fa-pen-to-square fa-xl editIcon"></i>
-                                <p>Edit</p>
-                            </div>
-                        </a>
-                    </div>
+                    </a>
                 </div>
+            </div>
 
             <?php
-                }
+            }
 
             ?>
-        </div>
+    </div>
     </div>
 </body>
+
 </html>
